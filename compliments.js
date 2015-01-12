1156bbaf3cda44d4f527766d44c5eea419684d93
Tom <tomknabe@gmail.com>
2015-01-12 09:44:23 -0500

    Create compliments.js

---
+++ compliments.js
@@ -0,0 +1,14 @@
+[
+      'You look nice today',
+      'Fancy seeing you here',
+      'Have you been working out?',
+      'Hello, good looking.',
+      'I would share my dessert with you.',
+      'I feel the need to impress you.',
+      'You could probably lead a rebellion.',
+      'Being awesome is hard, but you\'ll manage.',
+      'My mom always asks me why I can\'t be more like you',
+      'The Force is strong with you.',
+      'You complete me',
+      'Glorious things are waiting for you.'
+]
